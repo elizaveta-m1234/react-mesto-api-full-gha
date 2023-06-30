ff5f80fbd77662a31493315bce129a238799e4f5
Elizaveta Miheevskaya <emikheevskaya@gmail.com>
2023-06-30 22:16:16 +0300

    1st review/4

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -73,7 +73,7 @@ function App() {
   }
 //работа с карточками
   function handleCardLike(card) {
-    const isLiked = card.likes.some(i => i._id === currentUser._id);
+    const isLiked = card.likes.some(i => i === currentUser._id);
 
     api.changeLikeCardStatus(card._id, !isLiked)
       .then((newCard) => {
